86e705415f5e1800bd08fa8b7eeb68a537cf8a60
Fatima Aftab <129087237+fatimaaftab1388@users.noreply.github.com>
2026-01-07 20:32:08 +0500

    fatima

--- AdvanceCreations/lib/firebase.ts
+++ AdvanceCreations/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCEzfDsTdqbrx88TibmvnaKm17XA5CcXMw",
-    authDomain: "advance-936c9.firebaseapp.com",
-    projectId: "advance-936c9",
-    storageBucket: "advance-936c9.firebasestorage.app",
-    messagingSenderId: "489721734370",
-    appId: "1:489721734370:web:bdd4bacdecbb12188c2abb",
-    measurementId: "G-BN5WZ7K4BE"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase (only once)
